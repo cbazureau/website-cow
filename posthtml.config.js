7f83fbe3cae371c46a860262610fd6da95271b11
Cédric Bazureau <cedric.bazureau@renault.com>
2022-06-25 13:47:01 +0200

    chore(init): init with cow-scripts 0.2.2

---
+++ posthtml.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  publicUrl: '/create-one-page-website/',
+  plugins: {
+    'cow-scripts/posthtml-cow-component': {
+      workingDir: __dirname,
+      manifest: {
+        short_name: 'Cédric B.',
+        name: 'Cédric Bazureau',
+        start_url: '/',
+        // icons key isn't needed
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+    },
+  },
+};
