75facef985b948a21644733bab099cb2eb87e61b
Cédric Bazureau <cedric.bazureau@renault.com>
2022-08-02 10:56:23 +0200

    fix(live): public url

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  publicUrl: '/create-one-page-website/',
-  extraFilesToCopy: ['.htaccess', 'robots.txt', 'sitemap.xml'],
+  publicUrl: "/",
+  extraFilesToCopy: [".htaccess", "robots.txt", "sitemap.xml"],
   plugins: {
-    'cow-scripts/posthtml-cow-component': {
+    "cow-scripts/posthtml-cow-component": {
       workingDir: __dirname,
       manifest: {
-        short_name: 'Cédric B.',
-        name: 'Cédric Bazureau',
-        start_url: '/',
+        short_name: "Cédric B.",
+        name: "Cédric Bazureau",
+        start_url: "/",
         // icons key isn't needed
-        display: 'standalone',
-        theme_color: '#000000',
-        background_color: '#ffffff',
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
       },
     },
   },
